526e17e1b320d9cf11dcf6566a2d7b3f64eb1d42
Vanessa Bou <rvbouu@gmail.com>
2024-04-24 18:01:49 -0500

    commented files

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+// declaring dependencies and variables
 const {makeLog} = require('./lib/log');
 const {questions} = require('./lib/questions')
 
+// function to initialize program
 function init(){
+  // calls questions function to prompt the user for input then passes the values to makeLog function
+  // makeLog function saves generated SVG file to examples folder
   questions().then((content) => makeLog('./examples/logo.svg', content));
 }
 
+// calls init() function to run the program
 init();
--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,18 +1,23 @@
+// importing class constructors from shapes.js
 const shape = require('../lib/shapes');
 
+// Testing suite for shapes package is created
 describe('Test of Shapes Package', () => {
+  // testing if circle.render does return `<circle cx="150" cy="100" r="80" fill="red" />` string with the color red in it
   it('should render a red circle', () => {
     const circle = new shape.Circle('red');
 
     expect(circle.render()).toEqual(`<circle cx="150" cy="100" r="80" fill="red" />`);
   })
 
+  // testing if triangle.render does return `<polygon points='150, 18 244, 182 56, 182' fill="blue" />` string
   it('should render a blue triangle', () => {
     const triangle = new shape.Triangle('blue');
 
     expect(triangle.render()).toEqual(`<polygon points='150, 18 244, 182 56, 182' fill="blue" />`);
   })
 
+  // testing to see if square.render does return `<rect width='200' height='200' x='10' y='10' fill="green" />`
   it('should render a green square', () => {
     const square = new shape.Square('green');
 
